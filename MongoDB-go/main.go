bb675ec00b8bd8e3061f2d66ccaa3d719fbc71cd
Pradeep Kumar <61448614+pradytpk@users.noreply.github.com>
2022-11-29 18:41:55 +0900

    updated the code

--- MongoDB-go/main.go
+++ MongoDB-go/main.go
@@ -19,7 +19,7 @@ func main() {
 }
 
 func getSession() *mgo.Session {
-	s, err := mgo.Dial("mongodb://localhost:27017")
+	s, err := mgo.Dial("mongodb://localhost:27017/")
 	if err != nil {
 		panic(err)
 	}
